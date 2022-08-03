d2103223edb359cea29f0d29f7ecbb17e6ee5e74
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-08-03 16:25:36 -0500

    Add error handling

--- src/App.js
+++ src/App.js
@@ -9,14 +9,19 @@ class App extends React.Component {
     super(props);
     this.state = {
       searchEntry: '',
+      //LocationIQ varaiables
       locationData: { data: [{}] },
-      forecast: { data: [] },
       latitude: '',
       longitude: '',
       mapURL: '',
       hideCoordinates: true,
-      noError: true,
-      errorMessage: ''
+      locationSuccessful: true,
+      locationErrorMessage: '',
+      //Weather variables
+      forecast: { data: [] },
+      showWeather: false,
+      weatherError: false,
+      weatherErrorMessage: ''
     };
   }
 
@@ -28,30 +33,46 @@ class App extends React.Component {
 
   submitLocationHandler = async (event) => {
     event.preventDefault();
+    let locationData
     try {
       let requestURL = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&q=${this.state.searchEntry}&format=json`;
-      let locationData = await axios.get(requestURL);
+      locationData = await axios.get(requestURL);
       let latitude = locationData.data[0].lat > 0 ? `${locationData.data[0].lat}° N` : `${Math.abs(locationData.data[0].lat)}° S`;
       let longitude = locationData.data[0].lon > 0 ? `${locationData.data[0].lon}° E` : `${Math.abs(locationData.data[0].lon)}° W`;
       let mapURL = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&center=${locationData.data[0].lat},${locationData.data[0].lon}&zoom=13&size=1000x1000`;
-      let forecastURL = `http://localhost:3001/forecast?search=${this.state.searchEntry}&lat=${locationData.data[0].lat}lon=${locationData.data[0].lon}`;
-      let forecastReturn = await axios.get(forecastURL);
-      console.log(forecastReturn);
       this.setState({
         locationData: locationData,
-        forecast: forecastReturn,
         latitude: latitude,
         longitude: longitude,
         mapURL: mapURL,
         hideCoordinates: false,
-        noError: true
+        locationSuccessful: true
       })
     } catch (error) {
-      console.log('error', error);
+      console.log('location error', error);
       this.setState({
         hideCoordinates: true,
-        noError: false,
-        errorMessage: `${error.message}: ${error.response.data.error}`
+        locationSuccessful: false,
+        locationErrorMessage: `${error.message}: ${error.response.data.error}`,
+        forecast: { data: [] },
+      })
+    }
+    try {
+      let forecastURL = `${process.env.REACT_APP_SERVER_KEY}forecast?search=${this.state.searchEntry}&lat=${locationData.data[0].lat}lon=${locationData.data[0].lon}`;
+      let forecastReturn = await axios.get(forecastURL);
+      this.setState({
+        forecast: forecastReturn,
+        showWeather: true,
+        weatherError: false,
+        weatherErrorMessage: ''
+      })
+    } catch (error) {
+      console.log('weather error', error);
+      this.setState({
+        forecast: { data: [] },
+        showWeather: false,
+        weatherError: true,
+        weatherErrorMessage: `${error.message}: ${error.response.data}`,
       })
     }
   }
@@ -75,8 +96,8 @@ class App extends React.Component {
             <Card.Text>{this.state.latitude}, {this.state.longitude}</Card.Text>
             <Card.Img variant='bottom' src={this.state.mapURL} />
           </Card>
-          <Badge bg='danger' hidden={this.state.noError} >{this.state.errorMessage}</Badge>
-          <Weather hideCoordinates={this.state.hideCoordinates} forecast={this.state.forecast}/>
+          <Badge bg='danger' hidden={this.state.locationSuccessful} >{this.state.locationErrorMessage}</Badge>
+          <Weather showWeather={this.state.showWeather} weatherError={this.state.weatherError} weatherErrorMessage={this.state.weatherErrorMessage} forecast={this.state.forecast} />
         </main>
         <footer>&copy;2022 Daniel Frey</footer>
       </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Badge } from 'react-bootstrap';
 
 class Weather extends React.Component {
   render() {
-    return (
-      <div id='forecast' hidden={this.props.hideCoordinates} >
-        {this.props.forecast.data.map((day, index) => {
+    let weatherDisplay = (
+      this.props.showWeather ?
+        this.props.forecast.data.map((day, index) => {
           return (
             <Card key={index}>
               <Card.Title>{day.date}</Card.Title>
               <Card.Body>{day.description}</Card.Body>
             </Card>
           );
-        })}
+        })
+        : this.props.weatherError ?
+          <Badge bg='danger' >{this.props.weatherErrorMessage}</Badge>
+          : <></>
+    )
+    return (
+      <div id='forecast'>
+        {weatherDisplay}
       </div>
     )
   }
